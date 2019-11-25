7638f25d3290161d7572dae88f63c8a41ed18338
alumno informatica <alumno@ciis.usil.edu.pe>
2019-11-25 13:53:57 -0500

    cambios

--- Models/Menu.cs
+++ Models/Menu.cs
@@ -6,7 +6,7 @@ namespace PC02.Models
         public string Entrada { get; set; }
         public string Plato { get; set; }
         public string Bebida { get; set; }
-        public Datetime Fecha { get; set; }
+        public DateTime Fecha { get; set; }
 
     }
 }
