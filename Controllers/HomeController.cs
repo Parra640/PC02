303054e1c90587de3300e85ac1a06862695dfda2
AdeGabriela1 <adelaotayzacp@gmail.com>
2019-11-25 16:56:41 -0500

    cambios varios

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -59,6 +59,8 @@ namespace PC02.Controllers
             return View();
         }
 
+        [HttpPost]
+        [Authorize(Roles="admin")]
          public IActionResult Registro(Menu x)
         {
                   if(ModelState.IsValid){
@@ -67,14 +69,137 @@ namespace PC02.Controllers
                  return RedirectToAction("Index");
              }
 
+            return View(x);
+        }
+
+        [Authorize(Roles="admin")]
+        public IActionResult AsociarRol()
+        {
+            ViewBag.Usuarios = _um.Users.ToList();
+            ViewBag.Roles = _rm.Roles.ToList();
             return View();
         }
 
+        [Authorize(Roles="admin")]
+        [HttpPost]
+        public IActionResult AsociarRol(string usuario, string rol)
+        {
+            var user = _um.FindByIdAsync(usuario).Result;
 
-
-        public IActionResult AgregarMenu(){
-
+            var resultado = _um.AddToRoleAsync(user, rol).Result;
+            
+            TempData["mensaje"]="Categoria asociada con éxito";
+            TempData["tipoTexto"]="text-success";
+            ViewBag.Usuarios = _um.Users.ToList();
+            ViewBag.Roles = _rm.Roles.ToList();
             return View();
+        }
+
+        [Authorize(Roles="admin")]
+        public IActionResult CrearRol()
+        {
+            return View();
+        }
+
+        
+        [Authorize(Roles="admin")]
+        [HttpPost]
+        public IActionResult CrearRol(string nombre)
+        {
+            var rol = new IdentityRole();
+            rol.Name = nombre;
+
+            var resultado = _rm.CreateAsync(rol).Result;
+
+            if(resultado.Succeeded){
+                TempData["mensaje"]="Rol creado con éxito";
+                TempData["tipoTexto"]="text-success";
+            }else{
+                TempData["mensaje"]="No se pudo crear el rol";
+                TempData["tipoTexto"]="text-danger";
+            }
+            return View();
+        }
+
+        
+        public IActionResult CrearCuenta()
+        {
+            return View();
+        }
+
+        public IActionResult AccesoDenegado()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CrearCuenta(CrearCuentaViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // Guardar datos del modelo en la tabla usuarios
+                var usuario = new Usuario();
+                usuario.UserName = model.Correo;
+                usuario.Correo = model.Correo;
+
+                IdentityResult resultado = _um.CreateAsync(usuario, model.Password1).Result;
+                var r = _um.AddToRoleAsync(usuario, "usuario").Result;
+
+                if (resultado.Succeeded)
+                {
+                    TempData["mensaje"]="Cuenta creada con éxito";
+                    TempData["tipoTexto"]="text-success";
+                    return View();
+                }
+                else
+                {
+                    TempData["mensaje"]="Error. No se pudo crear a cuenta";
+                    TempData["tipoTexto"]="text-danger";
+                    foreach (var item in resultado.Errors)
+                    {
+                        ModelState.AddModelError("", item.Description);
+                    }
+                }
+            }
+
+            return View(model);
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginViewModel model)
+        {
+
+            if (ModelState.IsValid)
+            {
+
+
+                var resultado = _sim.PasswordSignInAsync(model.Correo, model.Password, true, false).Result;
+
+                if (resultado.Succeeded)
+                {
+
+                    return RedirectToAction("index", "home");
+                }
+                else
+                {
+                    TempData["mensaje"]="Error. Datos incorrectos";
+                    TempData["tipoTexto"]="text-danger";
+                    ModelState.AddModelError("", "Datos incorrectos");
+                }
+            }
+
+            return View(model);
+        }
+
+        public IActionResult Logout()
+        {
+            _sim.SignOutAsync();
+            return RedirectToAction("index", "home");
         }
 
 
---
+++ Models/CrearCuentaViewModel.cs
@@ -0,0 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace PC02.Models
+{
+    public class CrearCuentaViewModel
+    {
+        [Required]
+        [Display(Name ="Correo Electrónico")]
+        public string Correo{get; set;}    
+
+        [Required]
+        [Display(Name="Contraseña")]
+        [DataType(DataType.Password)]
+        public string Password1 { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name="Confirmar Contraseña")]
+        [Compare("Password1", ErrorMessage = "Las contraseñas no coinciden")]
+        public string Password2 { get; set; }
+    }
+}
